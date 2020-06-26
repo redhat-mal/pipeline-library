7fe02f539076c299f0b8501d44dd3321d4af0858
Michael LaCourse <mlacours@redhat.com>
2020-06-26 11:20:11 -0400

    smoke

--- vars/stage_qg6_smoke_test.groovy
+++ vars/stage_qg6_smoke_test.groovy
@@ -34,7 +34,7 @@ def call() {
 				echo 'Identify the Port and the Url of the docker container'
 				//def inspectCmd = 'docker inspect --format=' + '\'' + '{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' + '\' ' + SERVICE_NAME
 				APP_PORT =  "8080"
-                                def svcCmd = 'oc create svc clusterip' + SERVICE_NAME + '-smoke --tcp=8080:8080'
+                                def svcCmd = 'oc create svc clusterip ' + SERVICE_NAME + '-smoke --tcp=8080:8080'
                                 sh svcCmd
 				APP_URL = "http://" + SERVICE_NAME + "-smoke:" + APP_PORT
 				println("OCP Container - " + SERVICE_NAME + " - Application Url: " + APP_URL);
